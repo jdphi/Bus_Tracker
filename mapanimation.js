9a0d55c90970a60caa320e68723bfb1e0a107731
Jeremy Phipps <jdphi123@gmail.com>
2022-11-27 13:44:45 -0700

    change bus stops to Object array

--- mapanimation.js
+++ mapanimation.js
@@ -1,20 +1,110 @@
 // This array contains the coordinates for all bus stops between MIT and Harvard
-const busStops = [
-    [-71.093729, 42.359244],
-    [-71.094915, 42.360175],
-    [-71.0958, 42.360698],
-    [-71.099558, 42.362953],
-    [-71.103476, 42.365248],
-    [-71.106067, 42.366806],
-    [-71.108717, 42.368355],
-    [-71.110799, 42.369192],
-    [-71.113095, 42.370218],
-    [-71.115476, 42.372085],
-    [-71.117585, 42.373016],
-    [-71.118625, 42.374863],
-  ];
-  
-  // TODO: add your own access token
+
+  const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-71.093729, 42.359244]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'busStopA'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-71.094915, 42.360175]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'BusStopB'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-71.0958, 42.360698]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'busStopC'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-71.099558, 42.362953]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'busStopD'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-71.103476, 42.365248]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'busStopE'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-71.106067, 42.366806]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'busStopF'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-71.108717, 42.368355]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'busStopG'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-71.110799, 42.369192]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'busStopH'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-71.113095, 42.370218]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'busStopI'
+        }
+      }
+    ]
+  };
+
   mapboxgl.accessToken = 'pk.eyJ1IjoiamRwaGkxMjMiLCJhIjoiY2xhb29kMnRuMHkwejQwcXRoNnhjNHRyMCJ9.NzKH_MkBN70QAv6zdRNqow';
   
   // This is the map instance
@@ -25,16 +115,24 @@ const busStops = [
     zoom: 14,
   });
   
-  var marker = new mapboxgl.Marker()
-        .setLngLat(-71.093729, 42.359244)
-        .addTo(map)
+  var marker = new mapboxgl.Marker().setLngLat(-71.093729, 42.359244).addTo(map)
+
+  // add markers to map
+for (const feature of geojson.features) {
+    // create a HTML element for each feature
+    const el = document.createElement('div');
+    el.className = 'marker';
+  
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
+  }
 
   // counter here represents the index of the current bus stop
   let counter = 0;
   function move() {
     setTimeout(() => {
-        if (counter >= busStops.length) return;
-        marker.setLngLat(busStops[counter]);
+        if (counter >= geojson.features.length) return;
+        marker.setLngLat(geojson.features[counter]);
         counter++;
         move();
       }, 1000);
